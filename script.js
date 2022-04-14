a8bb1153c2b4a07bcb9906a9ddc700ec6797d10d
Marlon Almino <alminomarlon@gmail.com>
2022-04-14 14:05:03 -0300

    created function mudarJogador( )

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let jogador = null
+let jogadorSelecionado = document.querySelector('#jogador-selecionado')
+
+mudarJogador('X')
 
 function escolherQuadrado(id) {
-  console.log(id)
+  let quadrado = document.getElementById(id)
+
+  quadrado.innerHTML = jogador
+  quadrado.style.color = '#000'
+}
+
+function mudarJogador(valor) {
+  jogador = valor
+  jogadorSelecionado.innerHTML = jogador
 }
